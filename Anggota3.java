faf53c10020d83f045c863ffd2cd2d9b721a6f77
renorizky29 <renorizkyyy29@gmail.com>
2025-12-18 19:25:43 +0700

    commit 1 : perkenalan diri

---
+++ Anggota3.java
@@ -0,0 +1,11 @@
+public class Anggota3 {
+    public static void main(String[] args) {
+        String nama = "Reno Rizky";
+        String nim = "1324076";
+        String jurusan = "Sistem Informasi Industri Otomotif";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
